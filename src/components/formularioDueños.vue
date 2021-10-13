5738c8eb35df739579dc068e566e74e2356b3948
Edovalm <edoval2680@gmail.com>
2021-10-12 19:36:25 -0500

    Commit inicial formulario y tabla para dueños

---
+++ src/components/formularioDueños.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="formDueños">
+
+
+<fieldset>
+    <legend>Formulario Registro Dueños</legend>
+        <form action="">
+
+            <label for="idDueño">id de dueños</label><br>
+            <input type="text" name="idDueño" id="idDueño" v-model="dueño.idDueño"><br>
+
+            <label for="nombre">nombre</label><br>
+            <input type="text" name="nombreDueño" id="nombre_dueño" v-model="dueño.nombreDueño"><br>
+
+            <label for="apellidos">Imagen</label><br>
+            <input type="text" name="imgDueño" id="img_dueño" v-model="dueño.imgDueño"><br>
+
+            <button type="submit" v-on:click="guardar()">Guardar</button>
+            <button type="submit" v-on:click="eliminar(dueño.idDueño)">Eliminar</button>
+        </form>
+</fieldset>
+    </div>
+
+
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name:'formularioDueños',
+data:function (){
+return{
+dueño: {
+idDueño:"",
+nombreDueño:"",
+imgDueño: ""
+}
+}
+},
+methods:{
+guardar(){
+
+axios.post('http://localhost:8080/api/dueños',this.dueño)
+.then(data=>{
+
+console.log(data);
+
+})
+
+},eliminar(id){
+
+    
+    axios.delete('http://localhost:8080/api/dueños/'+id)
+    .then(data=>{
+
+console.log(data);
+
+})
+//location.reload()  
+}
+}
+}
+
+</script>
+
+
+<style scoped>
+
+.formDueños{
+
+text-align: center;
+padding: 5%;
+font-family : inherit;
+
+}
+
+
+
+</style>
---
+++ src/components/tablaDueños.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="clas">
+  
+
+        <fieldset>
+            <legend>Tabla de Dueños</legend>
+<table>
+  <tr>
+    <th>id</th>
+    <th>nombre</th>
+    <th>Imagen</th>
+  </tr>
+  <tr v-for="dueño in dueños" :key="dueño.idDueño">
+    <td>{{dueño.idDueño}}</td>
+    <td>{{dueño.nombreDueño}}</td>
+    <td>{{dueño.imgDueño}}</td>
+  </tr>
+  
+</table>
+        </fieldset>
+
+        <router-view/>
+    </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+export default{
+name:'tablaDueños',
+data(){
+  return{
+     dueños:[]
+  }
+},
+mounted(){
+  let vue=this;
+  axios.get('http://localhost:8080/api/dueños/listar')
+  .then( response =>{
+
+    vue.dueños=response.data;
+  })
+},methods:{
+
+
+  
+}
+
+}
+
+</script>
+
+<style scoped>
+
+.tablaDueños{
+padding: 5%;
+text-align: center;
+
+}
+table{
+    
+    border: 1px solid #000;
+    margin: auto;
+    padding: 1 px;
+}
+th{
+
+border: 1px solid #000;
+
+}
+tr,td{
+    
+    width: 20%;
+   text-align: left;
+   vertical-align:top;
+   border: 1px solid #000;
+}
+</style>
